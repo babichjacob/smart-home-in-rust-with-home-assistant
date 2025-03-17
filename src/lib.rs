2a99e67d7c1eb67bf3af6b6d4a3391a34c30a2b2
Jacob <jacob@katniss.top>
2025-03-17 15:47:56 -0400

    feat: log the datetime the crate was built to prevent confusion

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@ use std::time::Duration;
 
 use home_assistant::home_assistant::HomeAssistant;
 use pyo3::prelude::*;
+use shadow_rs::shadow;
 use tokio::time::interval;
 use tracing::Level;
 use tracing_subscriber::fmt::format::FmtSpan;
@@ -10,12 +11,17 @@ mod arbitrary;
 mod home_assistant;
 mod python_utils;
 
+shadow!(build_info);
+
 async fn real_main(home_assistant: HomeAssistant) -> ! {
     tracing_subscriber::fmt()
         .with_max_level(Level::TRACE)
         .with_span_events(FmtSpan::ACTIVE)
         .pretty()
         .init();
+
+    let built_at = build_info::BUILD_TIME;
+    tracing::info!(built_at);
 
     let duration = Duration::from_millis(5900);
     let mut interval = interval(duration);
